e7ce64b9f18bcedf77b0c0d495dc36495bbc86f1
EvgeniyS <xjamsex@gmail.com>
2023-11-20 11:20:14 +0300

    Refactor DataBaseHelper

--- src/test/java/ru/netology/data/DataBaseHelper.java
+++ src/test/java/ru/netology/data/DataBaseHelper.java
@@ -1,60 +1,39 @@
 package ru.netology.data;
 
 import lombok.SneakyThrows;
-import lombok.val;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class DataBaseHelper {
-    private final QueryRunner runner = new QueryRunner();
-    private final Properties prop = prop();
-    private final Connection conn = getConnect();
 
+    private static final QueryRunner runner = new QueryRunner();
 
-    private Properties prop() {
-        Properties properties = new Properties();
-        try (InputStream is = DataBaseHelper.class.getClassLoader().getResourceAsStream("application.properties")) {
-            properties.load(is);
-        } catch (IOException ex) { ex.printStackTrace(); }
-        return properties;
+    private DataBaseHelper() {
     }
 
     @SneakyThrows
-    private Connection getConnect() {
-        return DriverManager.getConnection(
-                prop.getProperty("spring.datasource.url"),
-                prop.getProperty("spring.datasource.username"),
-                prop.getProperty("spring.datasource.password")
-        );
+    public static Connection getConn() {
+        String dbUrl = System.getProperty("db.url");
+        String login = System.getProperty("login");
+        String password = System.getProperty("password");
+        return DriverManager.getConnection (dbUrl, login, password);
     }
-    @SneakyThrows
-    public String getPaymentStatus() {
-        val status = "SELECT status FROM payment_entity ORDER BY created DESC";
 
-        return runner.query(conn, status, new ScalarHandler<>());
-    }
     @SneakyThrows
-    public Integer getPaymentAmount() {
-        val amount = "SELECT amount FROM payment_entity ORDER BY created DESC";
-
-        return runner.query(conn, amount, new ScalarHandler<>());
+    public static DataHelper.StatusPayment getStatusPayment() {
+        String statusSQL = "SELECT status FROM payment_entity";
+        Connection conn = getConn();
+        return runner.query(conn, statusSQL, new BeanHandler<>(DataHelper.StatusPayment.class));
     }
+
     @SneakyThrows
-    public String getCreditRequestStatus() {
-        val status = "SELECT status FROM credit_request_entity ORDER BY created DESC";
-
-        return runner.query(conn, status, new ScalarHandler<>());
+    public static DataHelper.StatusCredit getStatusCredit() {
+        String statusSQL = "SELECT status FROM credit_request_entity";
+        Connection conn = getConn();
+        return runner.query(conn, statusSQL, new BeanHandler<>(DataHelper.StatusCredit.class));
     }
-    @SneakyThrows
-    public String getCreditId() {
-        val id = "SELECT credit_id FROM credit_request_entity ORDER BY created DESC";
 
-        return runner.query(conn, id, new ScalarHandler<>());
-    }
 }
