a73d690b881ab0881b6211dd25fb26a7d5947a9c
EvgeniyS <xjamsex@gmail.com>
2023-11-20 11:23:48 +0300

    Test added and refactor Credit and Payment

--- src/test/java/ru/netology/test/CreditTest.java
+++ src/test/java/ru/netology/test/CreditTest.java
@@ -2,19 +2,18 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Epic;
 import io.qameta.allure.selenide.AllureSelenide;
-import lombok.val;
 import org.junit.jupiter.api.*;
 import ru.netology.data.DataHelper;
 import ru.netology.data.DataBaseHelper;
-import ru.netology.page.OrderPage;
+import ru.netology.page.CreditPage;
+import ru.netology.page.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-
-public class CreditRequestTest {
+public class CreditTest {
 
     @BeforeAll
     static void setUpAll() {
@@ -22,65 +21,283 @@ public class CreditRequestTest {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
     }
-
     @BeforeEach
-    void setUp() {
-        open("http://localhost:8080");
+    public void setUp() {
+        open("http://localhost:8080/");
     }
 
     @AfterAll
-    static void tearDownAll() {
+    public static void tearDownAll() {
         SelenideLogger.removeListener("allure");
-        DataHelper.cleanData();
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте со статусом APPROVED")
     @Test
-    void shouldCreditApprovedCard() {
-        val cardInfo = new DataHelper().getValidCardInfo("approved");
-        val creditPage = new OrderPage().openBuyCredit();
+    public void shouldConfirmCreditApprovedCard()  {
+        MainPage mainPage = new MainPage();
 
-        creditPage.credit(cardInfo);
-        creditPage.approved();
+        var CardInfo = DataHelper.getValidCard();
 
-        assertEquals("APPROVED", new DataBaseHelper().getCreditRequestStatus());
-        assertNull(new DataBaseHelper().getCreditId());
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(CardInfo);
+        creditPage.getSuccessNotificationCredit();
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusCredit());
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте с текущим сроком действия")
     @Test
-    void shouldPaymentDeclinedCard() {
-        val cardInfo = new DataHelper().getValidCardInfo("declined");
-        val creditPage = new OrderPage().openBuyCredit();
+    public void shouldConfirmCreditWithCurrentMonthAndYear()  {
+        MainPage mainPage = new MainPage();
 
-        creditPage.credit(cardInfo);
-        creditPage.declined();
+        var CardInfo = DataHelper.getCurrentMonthAndYear();
 
-        assertEquals("DECLINED", new DataBaseHelper().getCreditRequestStatus());
-        assertNull(new DataBaseHelper().getCreditId());
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(CardInfo);
+        creditPage.getSuccessNotificationCredit();
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusCredit());
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте со статусом DECLINED")
     @Test
-    void shouldGetNotificationInvalidCard() {
-        val cardInfo = new DataHelper().getInvalidCardInfo("approved");
-        val creditPage = new OrderPage().openBuyCredit();
+    public void shouldNotCreditDeclinedCard()  {
+        MainPage mainPage = new MainPage();
 
-        creditPage.credit(cardInfo);
-        creditPage.invalidCardNotification();
+        var CardInfo = DataHelper.getDeclinedCard();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(CardInfo);
+        creditPage.getErrorNotificationCredit();
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusCredit());
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Все поля пустые")
     @Test
-    void shouldGetNotificationWrongFormatCard() {
-        val cardInfo = new DataHelper().getInvalidFormatCard("4444");
-        val creditPage = new OrderPage().openBuyCredit();
+    public void shouldNotCreditEmptyForm()  {
+        MainPage mainPage = new MainPage();
 
-        creditPage.credit(cardInfo);
-        creditPage.wrongFormatNotification();
+        var emptyCardInformation = DataHelper.getAllFieldsEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(emptyCardInformation);
+        creditPage.getInvalidFormatCredit();
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Номер карты")
     @Test
-    void shouldGetNotificationEmptyFields() {
-        val creditPage = new OrderPage().openBuyCredit();
+    public void shouldNotCreditEmptyCard()  {
+        MainPage mainPage = new MainPage();
 
-        creditPage.emptyFieldNotification();
+        var fieldCardEmpty = DataHelper.getCardNumberEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(fieldCardEmpty);
+        creditPage.getInvalidFormatCredit();
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Месяц")
+    @Test
+    public void shouldNotCreditEmptyMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldMonthEmpty = DataHelper.getMonthEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(fieldMonthEmpty);
+        creditPage.getInvalidFormatCredit();
+
+    }
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Год")
+    @Test
+    public void shouldNotCreditEmptyYear()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldYearEmpty = DataHelper.getYearEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(fieldYearEmpty);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Владелец")
+    @Test
+    public void shouldNotCreditEmptyHolder()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldHolderEmpty = DataHelper.getHolderEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(fieldHolderEmpty);
+        creditPage.getRequiredFieldCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле CVV")
+    @Test
+    public void shouldNotCreditEmptyCvv()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldCvvEmpty = DataHelper.getCVVEmpty();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(fieldCvvEmpty);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидный номер карты")
+    @Test
+    public void shouldNotCreditInvalidNumber()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidNumber();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидное значение в поле Месяц")
+    @Test
+    public void shouldNotCreditWrongMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidMonth();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidDateCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидное значение в поле Год")
+    @Test
+    public void shouldNotCreditWrongYear()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getWrongYear();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidDateCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Цифры в поле Владелец")
+    @Test
+    public void shouldNotCreditNumericHolder()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getNumericName();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Неверный формат CVV-код")
+    @Test
+    public void shouldNotCreditInvalidCVV()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidCVV();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Истёкший месяц карты")
+    @Test
+    public void shouldNotCreditExpiredMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getExpiredMonth();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidDateCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Истёкший год карты")
+    @Test
+    public void shouldNotCreditExpiredYear()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getExpiredYear();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.expiredCreditYear();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле Номер карты - нули")
+    @Test
+    public void shouldNotCreditZeroNumber()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroCard();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidFormatCredit();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле Месяц значение - 0")
+    @Test
+    public void shouldNotCreditZeroMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroMonth();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidDateCredit();
+
+    }
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле CVV значение - 000")
+    @Test
+    public void shouldNotCreditZeroCVV()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroCVV();
+
+        CreditPage creditPage = mainPage.creditButtonClick();
+        creditPage.inputCreditData(invalidCard);
+        creditPage.getInvalidFormatCredit();
+
+    }
 }
--- src/test/java/ru/netology/test/PaymentTest.java
+++ src/test/java/ru/netology/test/PaymentTest.java
@@ -2,16 +2,16 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Epic;
 import io.qameta.allure.selenide.AllureSelenide;
-import lombok.val;
 import org.junit.jupiter.api.*;
 import ru.netology.data.DataHelper;
 import ru.netology.data.DataBaseHelper;
-import ru.netology.page.OrderPage;
+import ru.netology.page.MainPage;
+import ru.netology.page.PaymentPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PaymentTest {
     @BeforeAll
@@ -20,65 +20,286 @@ public class PaymentTest {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
     }
-
     @BeforeEach
-    void setUp() {
-        open("http://localhost:8080");
+    public void setUp() {
+        open("http://localhost:8080/");
     }
 
-    @AfterAll
-    static void tearDownAll() {
+     @AfterAll
+    public static void tearDownAll() {
         SelenideLogger.removeListener("allure");
-        DataHelper.cleanData();
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте со статусом APPROVED")
     @Test
-    void shouldPaymentApprovedCard() {
-        val cardInfo = new DataHelper().getValidCardInfo("approved");
-        val paymentPage = new OrderPage().goToPayment();
+    public void shouldConfirmPaymentApprovedCard() throws InterruptedException {
+        MainPage mainPage = new MainPage();
 
-        paymentPage.payment(cardInfo);
-        paymentPage.approved();
+        var CardInfo = DataHelper.getValidCard();
 
-        assertEquals("APPROVED",new DataBaseHelper().getPaymentStatus());
-        assertEquals(4500000, new DataBaseHelper().getPaymentAmount());
-        assertNull(new DataBaseHelper().getCreditId());
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(CardInfo);
+        paymentPage.getSuccessNotification();
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusPayment());
+        Thread.sleep(3000);
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте с текущим сроком действия")
     @Test
-    void shouldPaymentDeclinedCard() {
-        val cardInfo = new DataHelper().getValidCardInfo("declined");
-        val paymentPage = new OrderPage().goToPayment();
+    public void shouldConfirmPaymentCurrentMonthAndYear()  {
+        MainPage mainPage = new MainPage();
 
-        paymentPage.payment(cardInfo);
-        paymentPage.declined();
+        var validCardInformation = DataHelper.getCurrentMonthAndYear();
 
-        assertEquals("DECLINED", new DataBaseHelper().getPaymentStatus());
-        assertNull(new DataBaseHelper().getCreditId());
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(validCardInformation);
+        paymentPage.getSuccessNotification();
+
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusPayment());
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Покупка по карте со статусом DECLINED")
     @Test
-    void shouldGetNotificationInvalidCard() {
-        val cardInfo = new DataHelper().getInvalidCardInfo("approved");
-        val paymentPage = new OrderPage().goToPayment();
+    public void shouldNotPayDeclinedCard()  {
+        MainPage mainPage = new MainPage();
 
-        paymentPage.payment(cardInfo);
-        paymentPage.invalidCardNotification();
+        var declinedCard = DataHelper.getDeclinedCard();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(declinedCard);
+        paymentPage.getErrorNotification();
+
+        assertEquals("APPROVED", DataBaseHelper.getStatusPayment());
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Все поля пустые")
     @Test
-    void shouldGetNotificationWrongFormatCard() {
-        val cardInfo = new DataHelper().getInvalidFormatCard("4444");
-        val paymentPage = new OrderPage().goToPayment();
+    public void shouldNotPayEmptyForm()  {
+        MainPage mainPage = new MainPage();
 
-        paymentPage.payment(cardInfo);
-        paymentPage.wrongFormatNotification();
+        var emptyCardInformation = DataHelper.getAllFieldsEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(emptyCardInformation);
+        paymentPage.getInvalidFormatCard();
+
     }
 
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Номер карты")
     @Test
-    void shouldGetNotificationEmptyFields() {
-        val paymentPage = new OrderPage().goToPayment();
+    public void shouldNotPayEmptyCard()  {
+        MainPage mainPage = new MainPage();
 
-        paymentPage.emptyFieldNotification();
+        var fieldCardEmpty = DataHelper.getCardNumberEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(fieldCardEmpty);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Месяц")
+    @Test
+    public void shouldNotPayEmptyMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldMonthEmpty = DataHelper.getMonthEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(fieldMonthEmpty);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Год")
+    @Test
+    public void shouldNotPayEmptyYear()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldYearEmpty = DataHelper.getYearEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(fieldYearEmpty);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле Владелец")
+    @Test
+    public void shouldNotPayEmptyHolder()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldHolderEmpty = DataHelper.getHolderEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(fieldHolderEmpty);
+        paymentPage.getRequiredFieldCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Пустое поле CVV")
+    @Test
+    public void shouldNotPayEmptyCvv()  {
+        MainPage mainPage = new MainPage();
+
+        var fieldCvvEmpty = DataHelper.getCVVEmpty();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(fieldCvvEmpty);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидный номер карты")
+    @Test
+    public void shouldNotPayInvalidNumber()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidNumber();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидное значение в поле Месяц")
+    @Test
+    public void shouldNotPayWrongMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidMonth();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidDateCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Невалидное значение в поле Год")
+    @Test
+    public void shouldNotPayWrongYear()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getWrongYear();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidDateCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Цифры в поле Владелец")
+    @Test
+    public void shouldNotPayNumericHolder()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getNumericName();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Неверный формат CVV-код")
+    @Test
+    public void shouldNotPayInvalidCVV()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getInvalidCVV();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Истёкший месяц карты")
+    @Test
+    public void shouldNotPayExpiredMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getExpiredMonth();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidDateCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Истёкший год карты")
+    @Test
+    public void shouldNotPayExpiredYear()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getExpiredYear();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.expiredCardYear();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле Номер карты - нули")
+    @Test
+    public void shouldNotPayZeroNumber()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroCard();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidFormatCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле Месяц значение - 0")
+    @Test
+    public void shouldNotPayZeroMonth()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroMonth();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidDateCard();
+
+    }
+
+    @Epic(value = "UI-тесты")
+    @DisplayName("Поле CVV значение - 000")
+    @Test
+    public void shouldNotPayZeroCVV()  {
+        MainPage mainPage = new MainPage();
+
+        var invalidCard = DataHelper.getZeroCVV();
+
+        PaymentPage paymentPage = mainPage.paymentButtonClick();
+        paymentPage.inputData(invalidCard);
+        paymentPage.getInvalidFormatCard();
+
     }
 }
